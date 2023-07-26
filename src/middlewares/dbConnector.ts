3db5c0f5cdcb9a792505030ba73198e1ebb63f2f
Ankit Katheriya <ankit_katheriya@epam.com>
2023-07-26 11:41:34 +0530

    added nodemon

--- src/middlewares/dbConnector.ts
+++ src/middlewares/dbConnector.ts
@@ -1,21 +1,24 @@
 const Sequelize = require('sequelize')
 import config from "../config/dbConfig"
 
-const sequelize = new Sequelize(config.database, config.userName, config.password, {
-    host: config.host,
-    port: config.port,
-    dialect: config.dialect,
-    protocol: config.protocol,
-    dialectOptions: config.dialectOptions
-})
-
-const startConnection = sequelize.authenticate().then(
-    success => {
-        console.log('success', success)
-    },
-    error => {
-        console.log('error', error)
-    }
-)
+const startConnection = () => {
+    const connection = new Sequelize(config.database, config.userName, config.password, {
+        host: config.host,
+        port: config.port,
+        dialect: config.dialect,
+        protocol: config.protocol,
+        dialectOptions: config.dialectOptions
+    })
+    
+    connection.authenticate().then(
+        success => {
+            console.log('success', success)
+        },
+        error => {
+            console.log('error', error)
+        }
+    )
+    return connection
+}
 
 export default startConnection
